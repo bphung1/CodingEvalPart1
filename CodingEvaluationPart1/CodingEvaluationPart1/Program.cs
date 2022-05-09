adce2d6c61c44d39b59f9681d7d629e2ce7224ef
Bao Phung <b.giaphung@gmail.com>
2022-05-09 10:13:46 -0500

    submitting part 1 of evaluation

---
+++ CodingEvaluationPart1/CodingEvaluationPart1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace CodingEvaluationPart1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var first = StructuredString("foo", "bar", "baz"); //should return [foo][bar][baz]
+            Console.WriteLine(first);
+
+            var second = StructuredString(first, "zoop"); //should return [[foo][bar][baz]][zoop]
+            Console.WriteLine(second);
+        }
+
+        public static string StructuredString(params string[] str)
+        {
+            StringBuilder newStr = new StringBuilder();
+
+            foreach (var s in str)
+            {
+                newStr.Append($"[{s}]");                
+            }    
+            return newStr.ToString();
+        }
+    }
+}
